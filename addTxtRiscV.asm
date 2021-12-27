a494ebb22d6e86f93a311f0de616711c47a56cb8
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-12-27 22:23:21 +0100

    Description with in and out file name

--- addTxtRiscV.asm
+++ addTxtRiscV.asm
@@ -1,28 +1,46 @@
-#open the file
-	li a7, 1024 	#system call for file_open
-	la a0, fname 	#address of filename string
-	li a1, 0 	# flags: 0-read file
-	ecall 		#file descriptor of opened file in a0
+###################################
+#
+# Jakub Tomkiewicz
+# Index: 300183
+#
+# RiscV Project No. 21
+# Adding Text
+#
+####################################
 
-#save the file descriptor
+.data
+
+	inFileName:	.asciz	"source.bmp"
+	outFileName:	.asciz	"dest.bmp"
+
+.text
+
+main:
+# open the file
+	li a7, 1024 	# system call for file_open
+	la a0, fname 	# address of filename string
+	li a1, 0 	# flags: 0-read file
+	ecall 		# file descriptor of opened file in a0
+
+# save the file descriptor
 	mv s1, a0
 
-#check if file was opened correctly (file_descriptor<>-1)
-#...
+# check if file was opened correctly (file_descriptor<>-1)
+	#...
 
-#read data from file
-	li a7, 63	#system call for file_read
-	mv a0, s1	#move file descr from s1 to a0
-	la a1, buf	#address of data buffer
-	li a2, 4048	#amount to read (bytes)
+# read data from file
+	li a7, 63	# system call for file_read
+	mv a0, s1	# move file descr from s1 to a0
+	la a1, buf	# address of data buffer
+	li a2, 4048	# amount to read (bytes)
 	ecall
 
-#check how much data was actually read
-	beq zero,a0, fclose 	#branch if no data is read
-#...
+# check how much data was actually read
+	beq zero, a0, fclose 	# branch if no data is read
+	#...
 
-#close the file
+# close the file
 fclose:
-	li a7, 57	#system call for file_close
-	mv a0, s1	#move file descr from s1 to a0
+	li a7, 57	# system call for file_close
+	mv a0, s1	# move file descr from s1 to a0
 	ecall 
