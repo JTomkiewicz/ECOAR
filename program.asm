c00d3c0f46c965ef8baa254c98ad07d421670383
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-01-02 17:49:41 +0100

    Draw one

--- program.asm
+++ program.asm
@@ -174,13 +174,14 @@ goToLoop:
 #return: none
     la a3, input1 		# str = address of input buffer
     li s0, '0'
+    li s1, '1'
 
 whileLoop:    
     lbu t5, (a3)		# *str - store char in t1
     
     beq t5, zero, pastWhile 	# zero register always contains 0 * str == '\0'
     beq t5, s0, drawZero
-    #beq t1, '1', drawOne
+    beq t5, s1, drawOne
     #beq t1, '2', drawTwo
     #beq t1, '3', drawThree
     #beq t1, '4', drawFour
@@ -297,6 +298,112 @@ drawZero:
 	li	a0, 1
 	li	a1, 1
 	li 	a2, 0x00000000
+	jal	put_pixel
+	
+	addi a3, a3, 1 	     	# a0 = a0 + 1
+	b whileLoop		# go back to while loop
+	
+# ============================================================================
+drawOne:
+# print 1	
+	li	a0, 2		# x
+	li	a1, 0		# y
+	li 	a2, 0x00000000	# color - 00RRGGBB
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 0		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 0		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 5		
+	li	a1, 0		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 1		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 1		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 2		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 2		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 3		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 3		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 4		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 4		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 5		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 5	
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3	
+	li	a1, 6		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 6		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 3		
+	li	a1, 7		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 4		
+	li	a1, 7		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 1		
+	li	a1, 6		
+	li 	a2, 0x00000000	
+	jal	put_pixel
+	
+	li	a0, 2		
+	li	a1, 6		
+	li 	a2, 0x00000000	
 	jal	put_pixel
 	
 	addi a3, a3, 1 	     	# a0 = a0 + 1
