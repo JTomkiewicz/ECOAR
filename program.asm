7ef31e6160c8ccdacb4792b4d31f833ba5a23efd
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-01-04 15:32:23 +0100

    final comments v1

--- program.asm
+++ program.asm
@@ -1,12 +1,11 @@
-###################################
+# ============================================================================
 #
 # Jakub Tomkiewicz
 # Index: 300183
 #
-# RiscV Project No. 21
-# Adding Text
+# RISC-V Project No. 21 Adding Text
 #
-####################################
+# ============================================================================
 
 .eqv BMP_FILE_SIZE 230454	# 320 x 240 x 3 + 54
 .eqv BYTES_PER_ROW 960		# 320 x 3
@@ -41,39 +40,39 @@ main:
 	ecall
 	
 	# display the msg2
-	li a7, 4		
-	la a0, msg2		
+	li a7, 4		# system call for print_string
+	la a0, msg2		# address of string 
 	ecall
 	
-	# read the starting integer x position
-	li a7, 5		
+	# read starting integer (x position)
+	li a7, 5		# system call for read_int
 	ecall
-	mv a5, a0
+	mv a5, a0		# store int in a5
 	
-	# display the msg3
-	li a7, 4		
-	la a0, msg3		
+	# display msg3
+	li a7, 4		# system call for print_string
+	la a0, msg3		# address of string
 	ecall
 	
-	# read the starting integer y position
+	# read starting integer (y position)
 	li a7, 5		    		
 	ecall
-	mv a6, a0
+	mv a6, a0		# store int in a6
 	
 	# go to loop throught input1 string
     	la a3, input1		
-    	j goToLoop
+    	j goToLoop		# jump to loop
 
 saveAndExit:
-	# at the end save file
+	# at the end save bmp file
 	jal	save_bmp
 
-exit:	li 	a7,10		# terminate the program
+exit:	li 	a7,10		# terminate program
 	ecall
 
 # ============================================================================
 read_bmp:
-# description: reads the contents of a bmp file into memory
+# description: read content of a bmp file into memory
 # arguments: none
 # return: none
 	addi sp, sp, -4		# push $s1
@@ -104,7 +103,7 @@ read_bmp:
 
 # ============================================================================
 save_bmp:
-# description: saves bmp file stored in memory to a file
+# description: saves bmp file from memory to a bmp file
 # arguments: none
 # return: none
 	addi sp, sp, -4		# push s1
@@ -135,8 +134,8 @@ save_bmp:
 
 # ============================================================================
 put_pixel:
-#description: sets the color of specified pixel
-#arguments: a0 - x coordinate, a1 - y coordinate, a2 - 0RGB - pixel color
+#description: set the color of given pixel
+#arguments: a0 (x coordinate), a1 (y coordinate), a2 (0RGB - pixel color)
 #return: none
 
 	la t1, image		# adress of file offset to pixel array
@@ -169,43 +168,43 @@ put_pixel:
 # ============================================================================
 goToLoop:
 #description: loop throught text
-#arguments: a0 - address of 1st char of string
+#arguments: a3 (address of 1st char of the input1)
 #return: none
     la a3, input1 		# str = address of input buffer
 
 whileLoop:    
-    lbu t5, (a3)		# *str - store char in t1
+    lbu t5, (a3)		# *str - store char
     
     beq t5, zero, pastWhile 	# zero register always contains 0 * str == '\0'
-    li s0, '0'
-    beq t5, s0, drawZero
+    li s0, '0'			
+    beq t5, s0, drawZero	# if char is 0 then draw 0
     li s0, '1'
-    beq t5, s0, drawOne
+    beq t5, s0, drawOne		# if char is 1 then draw 1
     li s0, '2'
-    beq t5, s0, drawTwo
+    beq t5, s0, drawTwo		# if char is 2 then draw 2
     li s0, '3'
-    beq t5, s0, drawThree
+    beq t5, s0, drawThree	# if char is 3 then draw 3
     li s0, '4'
-    beq t5, s0, drawFour
+    beq t5, s0, drawFour	# if char is 4 then draw 4
     li s0, '5'
-    beq t5, s0, drawFive
+    beq t5, s0, drawFive	# if char is 5 then draw 5
     li s0, '6'
-    beq t5, s0, drawSix
+    beq t5, s0, drawSix		# if char is 6 then draw 6
     li s0, '7'
-    beq t5, s0, drawSeven
+    beq t5, s0, drawSeven	# if char is 7 then draw 7
     li s0, '8'
-    beq t5, s0, drawEight
+    beq t5, s0, drawEight	# if char is 8 then draw 8
     li s0, '9'
-    beq t5, s0, drawNine
+    beq t5, s0, drawNine	# if char is 9 then draw 9
     li s0, '.'
-    beq t5, s0, drawDot
+    beq t5, s0, drawDot		# if char is dot then draw dot
     
-    addi a3, a3, 1 	     	# a3 = a3 + 1
+    addi a3, a3, 1 	     	# go to next char
     
-    b whileLoop
+    b whileLoop			# go to the beginning of loop
     
 pastWhile:
-    j saveAndExit
+    j saveAndExit		# after the loop go save the bmp and exit
 
 # ============================================================================
 drawZero:
@@ -311,7 +310,7 @@ drawZero:
 	jal	put_pixel
 	
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -418,7 +417,7 @@ drawOne:
 	jal	put_pixel
 	
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -520,7 +519,7 @@ drawTwo:
 	jal	put_pixel
 	
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -647,7 +646,7 @@ drawThree:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -744,7 +743,7 @@ drawFour:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -846,7 +845,7 @@ drawFive:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 	# ============================================================================
@@ -958,7 +957,7 @@ drawSix:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -1065,7 +1064,7 @@ drawSeven:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 
 # ============================================================================
@@ -1192,7 +1191,7 @@ drawEight:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -1304,7 +1303,7 @@ drawNine:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
 	
 # ============================================================================
@@ -1391,5 +1390,5 @@ drawDot:
 	jal	put_pixel
 
 	addi a3, a3, 1 	     	# next char from string
-	addi a5, a5, 8		# print next letter at x+8
+	addi a5, a5, 8		# print next letter at x + 8
 	b whileLoop		# go back to while loop
