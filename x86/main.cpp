0a9a3c89f1ce8d8615c2b2407ac3f809db175d4e
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-02-02 18:51:17 +0100

    reading from user & begin read bmp

--- x86/main.cpp
+++ x86/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 
 // structure that stores information about image
 struct image
@@ -7,7 +8,7 @@ struct image
   int height;   // image heigth
   int width;    // image width
   int lineSize; // line size IN BYTES
-  char *img;
+  char *pImg;
 };
 
 // read from bmp file
@@ -15,8 +16,23 @@ image *readBmp(const char *fileName)
 {
   // create new image struct
   image *img;
+  // FILE pointer
+  FILE *pFile;
 
-  return img;
+  // rb stands for read binary
+  pFile = fopen(fileName, "rb");
+
+  // when file opened succesfully
+  if (pFile != NULL)
+  {
+    fclose(pFile);
+
+    return img;
+  }
+  else
+  {
+    return NULL;
+  }
 }
 
 // save to bmp file
@@ -33,21 +49,45 @@ int main(void)
 {
   // read source
   image *sourceImg = readBmp("source.bmp");
-
   // read numbers
   image *numbersImg = readBmp("numbers.bmp");
-
   // read destination
   image *destImg = readBmp("dest.bmp");
 
-  char inputText[] = "Wind On The Hill";
-  // printf("Input string      > %s\n", text);
-
-  if (func(sourceImg, numbersImg, destImg, inputText) != 0)
+  // check if files opened successfully
+  if (sourceImg == NULL || numbersImg == NULL || destImg == NULL)
   {
-    printf("Error in func.asm!\n");
+    std::cout << "Error while reading bmp fules!\n";
     return 1;
-  };
+  }
 
+  // read message & starting coordinates x and y
+  char message[15];
+  std::cout << "Input message to print (only numbers and dots are printed, other symbols will be ignored):\n";
+  std::cin >> message;
+
+  std::cout << "Input starting x (only integer allowed):\n";
+  int x;
+  std::cin >> x;
+
+  std::cout << "Input starting y (only integer allowed):\n";
+  int y;
+  std::cin >> y;
+
+  // run func.asm
+  if (func(sourceImg, numbersImg, destImg, message) != 0)
+  {
+    std::cout << "Error in func.asm!\n";
+    return 1;
+  }
+
+  // save image to dest.bmp
+  if (saveBmp("dest.bmp", destImg) != 0)
+  {
+    std::cout << "Error in saveBmp function!\n";
+    return 1;
+  }
+
+  // exit program
   return 0;
 }
