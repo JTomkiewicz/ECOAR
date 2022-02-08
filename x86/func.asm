42016e2f7f04f0ea8e57c0f8377906495c37eb50
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-02-08 20:53:12 +0100

    i and j functions in asm

--- x86/func.asm
+++ x86/func.asm
@@ -10,22 +10,58 @@
 ; func(image *numbersImg, image *srcImg, int startX, int startY, int numberX)
 
 section	.text
-global  func
+global func
 
 func:
-	push	ebp
+	push ebp
 	mov	ebp, esp
 
-; copy number from numbersImg at location [numberX, 0]
-mov eax, [ebp + 8] ; eax is address of the struct numbersImg 
+; create pointers & place them in proper place
+setupPointers: 
 
-; paste copied number into srcImg at location [startX, startY]
-mov eax, [ebp + 12] ; eax is address of the struct srcImg
+	mov eax, 0
+	mov [esp + 16], eax ; i = 0
 
-mov eax, [ebp + 16] ; eax is address of the int startX
-mov eax, [ebp + 20] ; eax is address of the int startY
-mov eax, [ebp + 24] ; eax is address of the int numbersX
+; for (int i = 0; i < 8; i++)
+loopI:
 
-exit:
+	mov eax, 8
+	cmp [esp + 16], eax ; i == 8
+	je afterLoopI
+
+	mov eax, 0 
+	mov [esp + 20], eax ; j = 0
+
+; for (int j = 0; j < 24; j++)
+loopJ:
+
+	mov eax, 24
+	cmp [esp + 20], eax ; j == 24
+	je afterLoopJ
+
+	mov eax, [esi] ; eax = *pNumbers
+	mov [edi], eax ; *pSrc = eax
+
+	mov eax, 4
+	add esi, eax ; pNumbers++
+	add edi, eax ; pSrc++
+
+	mov eax, 1
+	add [esp + 20], eax ;j++
+	jmp loopJ
+
+; when loopJ finishes, increment pScr and pNumbers
+afterLoopJ:
+
+	mov eax, 936 ; 960 - 24 (so line width - 3 * 8)
+	add esi, eax ; pNumbers += 936
+	add edi, eax ; pSrc += 936
+
+	mov eax, 1
+	add [esp + 16], eax ;j++
+	jmp loopI
+
+; when loopI finishes, exit
+afterLoopI:
 	pop	ebp
 	ret
